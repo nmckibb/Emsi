040d4ea59d5c20e4deb0912114ef3a3044f92f54
nmckibb <36752137+nmckibb@users.noreply.github.com>
2021-04-19 10:32:20 -0700

    Update test.py
    changed to put sql in var

--- test.py
+++ test.py
@@ -56,8 +56,10 @@ def CreateTable(conn):
 
 
 def insert_jobposting(conn, c, strbody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2):
-    c.execute("INSERT INTO tblJobPosting (body, title, expired, posted, state, city, onet, soc5, soc2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-       (strbody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2))
+    strSQL ="INSERT INTO tblJobPosting (body, title, expired, posted, state, city, onet, soc5, soc2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+       (strbody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2)
+    print (strSQL)
+    c.execute(strSQL)
 
 
 
