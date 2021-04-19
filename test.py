a93b805609c93763421bd3b53baf9d624bb2f7e1
nmckibb <36752137+nmckibb@users.noreply.github.com>
2021-04-19 10:34:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -56,8 +56,9 @@ def CreateTable(conn):
 
 
 def insert_jobposting(conn, c, strbody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2):
-    strSQL ="INSERT INTO tblJobPosting (body, title, expired, posted, state, city, onet, soc5, soc2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-       (strbody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2)
+    strSQL ="""INSERT INTO tblJobPosting 
+    (body, title, expired, posted, state, city, onet, soc5, soc2) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", (strbody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2)
     print (strSQL)
     c.execute(strSQL)
 
