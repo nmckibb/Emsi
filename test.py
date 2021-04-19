7eaf442c71d13e7fe3f7ddf6b393dcc1df552ee8
nmckibb <36752137+nmckibb@users.noreply.github.com>
2021-04-19 10:54:47 -0700

    Update test.py

--- test.py
+++ test.py
@@ -123,7 +123,8 @@ for i in f:
             strOnet = data["onet"]
             strSoc5 = dmos[data["onet"]]
             strSoc2 = "soc2"
-            insert_jobposting (conn, c, strBody, strTitle, dtExpired, dtPosted, strState, strCity, strOnet, strSoc5, strSoc2)
+            insert_jobposting (conn, c, strBody.strip(), strTitle.strip(), dtExpired.strip(), dtPosted.strip(),
+               strState.strip(), strCity.strip(), strOnet.strip(), strSoc5.strip(), strSoc2.strip())
             print strSoc5
             #print BeautifulSoup(data["body"],"lxml").text
             #print (data["onet"])
