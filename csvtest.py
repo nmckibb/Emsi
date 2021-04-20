a84a2215564a4d34653702b74daecfa0b574587e
nmckibb <36752137+nmckibb@users.noreply.github.com>
2021-04-19 20:13:23 -0700

    House Cleaning
    Doing House cleaning

--- csvtest.py
+++
@@ -1,9 +0,0 @@
-import csv
-with open('map_onet_soc.csv' ) as csvfile:
-  reader = csv.DictReader(csvfile)
-  for row in reader:
-  # print(row['onet'], row['soc5'])
-    
-
-    print(row)
-print (reader.get('99-9999.99'))
